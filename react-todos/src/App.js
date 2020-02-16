b3ad5a5a40d7858f7a96a463f19382b88426bb4e
Todd Skelton <todd.skelton@outlook.com>
2020-02-16 15:20:35 -0600

    Refactor out should reload.

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -3,12 +3,21 @@ import React, { useEffect, useState } from 'react';
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  const [shouldReload, setShouldReload] = useState({});
 
   async function getTodos() {
     const result = await fetch("/api/todos");
     const todos = await result.json();
     setTodos(todos);
+  }
+
+  async function createTodo(e) {
+    e.preventDefault();
+    await fetch('/api/todos', {
+      method: "POST",
+      body: JSON.stringify({ name: todo })
+    })
+    setTodo("");
+    await getTodos();
   }
 
   async function updateCompleted(todo, isComplete) {
@@ -18,52 +27,37 @@ function App() {
     });
   }
 
-  async function createTodo(todo) {
-    await fetch('/api/todos', {
-      method: "POST",
-      body: JSON.stringify(todo)
-    })
-    setTodo("");
-    setShouldReload({});
-  }
-
   async function deleteTodo(id) {
     await fetch(`/api/todos/${id}`, {
       method: "DELETE"
     });
-    setShouldReload({});
-  }
-
-  function submitTodoForm(event) {
-    event.preventDefault();
-    createTodo({ name: todo });
+    await getTodos();
   }
 
   useEffect(() => {
     getTodos();
-  }, [shouldReload]);
+  }, []);
 
   return (
-    <section class="todoapp">
-      <header class="header">
+    <section className="todoapp">
+      <header className="header">
         <h1>todos</h1>
-        <form onSubmit={submitTodoForm}>
-          <input class="new-todo" placeholder="What needs to be done?" value={todo || ""} onChange={(e) => setTodo(e.target.value)} />
+        <form onSubmit={createTodo}>
+          <input className="new-todo" placeholder="What needs to be done?" value={todo} onChange={(e) => setTodo(e.target.value)} />
         </form>
       </header>
-      <section class="main" style={{ display: "block" }}>
-        <input class="toggle-all" id="toggle-all" type="checkbox" />
-        <label for="toggle-all">Mark all as complete</label>
-        <ul class="todo-list">
+      <section className="main" style={{ display: "block" }}>
+        <input className="toggle-all" id="toggle-all" type="checkbox" />
+        <label htmlFor="toggle-all">Mark all as complete</label>
+        <ul className="todo-list">
           {todos.map(todo => {
             return (
               <li className={todo.IsComplete ? "completed" : ""} key={todo.id}>
-                <div class="view">
-                  <input class="toggle" type="checkbox" defaultChecked={todo.isComplete} onChange={(e) => updateCompleted(todo, e.target.checked)} />
+                <div className="view">
+                  <input className="toggle" type="checkbox" defaultChecked={todo.isComplete} onChange={(e) => updateCompleted(todo, e.target.checked)} />
                   <label>{todo.name}</label>
-                  <button class="destroy" onClick={() => deleteTodo(todo.id)}></button>
+                  <button className="destroy" onClick={() => deleteTodo(todo.id)}></button>
                 </div>
-                <input class="edit" value={todo.name} />
               </li>
             );
           })}
